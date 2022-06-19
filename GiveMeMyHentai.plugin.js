2a0afbdbb2369ee40e8d470d54e35eaa4a81690c
unknown <hapzazad@gmail.com>
2022-06-19 13:45:08 +0300

    fixed links to messages

--- GiveMeMyHentai.plugin.js
+++ GiveMeMyHentai.plugin.js
@@ -8,9 +8,17 @@
  */
 
 class GiveMeMyHentai {
+    MESSAGE_EVENTS = [
+        'MESSAGE_CREATE',
+        'MESSAGE_UPDATE',
+        'CHANNEL_SELECT',
+        'LOAD_MESSAGES_SUCCESS'
+    ];
+
     constructor() {
         this.initialized = false;
         this.last_channel = null;
+        this.listeners = [];
     }
 
     // Meta
@@ -40,7 +48,16 @@ class GiveMeMyHentai {
             if (this.last_channel) {
                 this.last_channel.nsfw = true;
             }
-            ZeresPluginLibrary.DiscordModules.Dispatcher.unsubscribe('CHANNEL_SELECT', this._callback);
+            let disp = ZeresPluginLibrary.DiscordModules.Dispatcher;
+            disp.unsubscribe('CHANNEL_SELECT', this._switch_callback);
+            for (let i = 0; i < this.MESSAGE_EVENTS.length; i++) {
+                disp.unsubscribe(this.MESSAGE_EVENTS[i], this._message_callback);
+            }
+
+            let pair;
+            while (pair = this.listeners.pop()) {
+                pair[0].removeEventListener('click', pair[1]);
+            }
         }
     }
 
@@ -54,22 +71,60 @@ class GiveMeMyHentai {
     }
 
     initialize() {
-        this._callback = (p) => this.switch_callback(p);
-        ZeresPluginLibrary.DiscordModules.Dispatcher.subscribe('CHANNEL_SELECT', this._callback);
+        this._switch_callback = (p) => this.switch_callback(p.channelId);
+        this._message_callback = () => setTimeout(() => this.message_callback(), 1000);
+        let disp = ZeresPluginLibrary.DiscordModules.Dispatcher;
+        disp.subscribe('CHANNEL_SELECT', this._switch_callback);
+        for (let i = 0; i < this.MESSAGE_EVENTS.length; i++) {
+            disp.subscribe(this.MESSAGE_EVENTS[i], this._message_callback);
+        }
         BdApi.showToast(this.getName() + " " + this.getVersion() + " has started.");
         this.initialized = true;
     }
 
-    switch_callback(payload) {
+    switch_callback(channel_id) {
         if (this.last_channel) {
             this.last_channel.nsfw = true;
             this.last_channel = null;
         }
-        if (!payload.channelId) return;
-        let channel = ZeresPluginLibrary.WebpackModules.getByProps('getChannel', 'getDMFromUserId').getChannel(payload.channelId);
-        if (channel.nsfw) {
+        if (!channel_id) return;
+        let channel = ZeresPluginLibrary.WebpackModules.getByProps('getChannel', 'getDMFromUserId').getChannel(channel_id);
+        if (channel && channel.nsfw) {
             channel.nsfw = false;
             this.last_channel = channel;
+        }
+    }
+
+    message_callback() {
+        let elements = document.getElementsByTagName("a");
+        for (let i = 0; i < elements.length; i++) {
+            let link = elements[i];
+            let match = link.href.match(/https?:\/\/discord(app)?.com\/channels\/\d+\/(?<id>\d+)/);
+            if (match) {
+                let callback = () => this.switch_callback(match.groups.id);
+                let already_injected = false;
+                for (let j = 0; j < this.listeners.length; j++) {
+                    if (this.listeners[j][0] === link) {
+                        already_injected = true;
+                        break;
+                    }
+                }
+                if (already_injected) continue;
+                link.addEventListener('click', callback);
+                this.listeners.push([link, callback]);
+            }
+        }
+    }
+
+    observer(changes) {
+        if (!changes.removedNodes) return
+        for (let i = 0; i < changes.removedNodes.length; i++) {
+            for (let j = 0; j < this.listeners.length; j++) {
+                if (changes.removedNodes[i].contains(this.listeners[j][0])) {
+                    this.listeners.splice(j, 1);
+                    j--;
+                }
+            }
         }
     }
 }
